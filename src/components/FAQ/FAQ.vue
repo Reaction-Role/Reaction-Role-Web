c36c90fa5e2cc057ebca172a2cbb71c35c0d271e
RumbleFrog <xxlosttechxx@gmail.com>
2018-06-25 08:29:06 -0400

    Update FAQ To Reflect Removal of Restriction

--- src/components/FAQ/FAQ.vue
+++ src/components/FAQ/FAQ.vue
@@ -1,35 +1,5 @@
 <template>
     <div>
-        <Message title="Reaction-role message limit per guild">
-            If you meet the limit,
-            you may see something like For cache performance purposes,
-            only patrons & supporters are allowed more than X messages
-            with reaction-role per guild. Also, if you recently deleted
-            a message that contains Reaction Roles, you will need to
-            wait for a garbage collector to delete it and this occurs
-            every 5 hours. The proper way to avoid this is to use the
-            deletion command instead.
-        </Message>
-
-        <Message title="Why's this restriction in place?">
-            Due to cache performances and the limited resources of the current machine,
-            the bot is on; the restriction is in place to
-            prevent one guild taking up all the resources,
-            degrading the performance for everyone else
-        </Message>
-
-        <Message title="How can I create more reaction-roles?">
-            As of current, you may vote 5 times to allocate more 5 messages
-            with reaction-role per guild, allocating up to 10 messages with reaction-role per guild
-        </Message>
-
-        <Message title="What if I want more than 10 reaction-roles?">
-            If you want more than 10 reaction-roles,
-            you may contribute to the hosting cost by becoming a Patron,
-            with the ability to allocate a mere infinite amount (except for abuses)
-            and Patron role on my support server!
-        </Message>
-
         <Message title="What's the group lock feature?" tag="Patron Only">
             The group lock feature is a system where you can lock a group of
             reaction-roles to only one available at a time.
