dc10608f61dee1ecdd32bc8c2299058aa2c54b42
RumbleFrog <xxlosttechxx@gmail.com>
2018-07-09 23:25:41 -0400

    [1.3.0] Removed Embed Link From Quickstart Deprecated

--- src/components/Documentation/Overview/Quickstart.vue
+++ src/components/Documentation/Overview/Quickstart.vue
@@ -32,16 +32,7 @@
                 Ensure <code>Reaction Role</code> role is above the roles
             </span>
         </div>
-
-        <div class="field">
-            <b-checkbox type="is-danger"></b-checkbox>
-            <span class="selectable-checkbox-text">
-                Ensure Reaction Role has
-                <code>Embed Link</code> permission on <b>BOTH</b> the channel you
-                are typing the command in <b>AND</b> the channel you are pointing at
-            </span>
-        </div>
-
+        
         <hr>
 
         <p>
